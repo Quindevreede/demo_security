9e9f635387fb82f3337614f18f9ca3da7648b81e
Quindevreede <quindevreede@hotmail.com>
2021-09-30 03:05:19 +0200

    security demo stap 1 en 2

---
+++ src/main/java/ml/novi/demo_security/controller/DemoController.java
@@ -0,0 +1,28 @@
+package ml.novi.demo_security.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+
+    @GetMapping("/")
+    public ResponseEntity<Object> demoHelloWorld() {
+        return ResponseEntity.ok("Security demo - Hello World");
+    }
+
+    @GetMapping("/secure")
+    public ResponseEntity<Object> demoSecure() {return ResponseEntity.ok("Security demo - ADMIN only"); }
+
+    @GetMapping("/users_only")
+    public ResponseEntity<Object> demoPublic() {
+        return ResponseEntity.ok("Security demo - USER and ADMIN access");
+    }
+
+    @GetMapping("/all")
+    public ResponseEntity<Object> demoEverybody() {
+        return ResponseEntity.ok("Landingspagina for everybody");
+    }
+
+}
